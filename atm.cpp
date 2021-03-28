c3548ad1c5cfface0fca2154587e33e2c03827d2
simone <sorzesimone1@gmail.com>
2021-03-28 17:44:18 +0200

    initial commit

---
+++ atm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void welcomeMenu();
+
+int main()
+{
+    welcomeMenu();
+    cout<<"hey"<<endl;
+    return 0;
+}
+
+void welcomeMenu(){
+    cout<<"connected"<<endl;
+}
